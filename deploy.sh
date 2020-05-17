118c1171398bce8ffb03fa5975c4bef88c11c1b1
Bohua Li <bohua.li.1984@gmail.com>
2020-05-17 23:21:49 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-curl --location --request POST 'http://ci-manager:3000/saas/api/v1/apps/c8df67ed-28db-441b-9fe0-09752364c7b8/import'
+curl --location --request POST 'http://ci-manager:3000/api/v1/apps/c8df67ed-28db-441b-9fe0-09752364c7b8/import'
